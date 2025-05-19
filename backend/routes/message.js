d729785c48976169ef60716c6d508eb5ac4c4373
pkc776 <pkc776.cs12@nycu.edu.tw>
2025-05-20 00:06:05 +0800

    feat:swaggerAPI doc

--- backend/routes/message.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendMessage, getMessages } = require('../controllers/messageController');
-
-router.post('/message', sendMessage);
-router.get('/message/:to', getMessages);
-
-module.exports = router;
--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,11 @@ const messageRoutes = require('./routes/message');
 app.use(userRoutes);
 app.use(messageRoutes);
 
+// Swagger UI 掛載
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
